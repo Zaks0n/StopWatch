3a436629c79c761fd3e1d889faa3e772e782a2c0
Zaks0n <zaksontech@gmail.com>
2017-10-01 21:53:01 +0100

    Initial commit

---
+++ app/src/main/java/com/zaksontech/www/stopwatch/MainActivity.java
@@ -0,0 +1,59 @@
+package com.zaksontech.www.stopwatch;
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.text.Format;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int seconds = 0;
+    private boolean running;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        timerRun();
+    }
+
+    public void startTimer(View view) {
+        running = true;
+    }
+
+    public void stopTimer(View view) {
+        running = false;
+    }
+
+    public void resetTimer(View view) {
+        running = false;
+        seconds = 0;
+    }
+
+    public void timerRun(){
+
+
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                TextView displayTimer = (TextView) findViewById(R.id.textView);
+                int hour = seconds/3600;
+                int minutes = (seconds%3600)/60;
+                int sec = seconds%60;
+
+                String time = String.format("%d:%02d:%02d",hour,minutes,sec);
+                displayTimer.setText(time);
+
+                if(running){
+                    seconds++;
+                }
+                handler.postDelayed(this, 1000);
+            }
+        });
+
+    }
+}
